02e3f65aee2f3c55c369f76e41472a38e870545b
hemantsharma-codes <golubhardwaj8126@gmail.com>
2026-01-06 16:28:06 +0530

    [Hemant Sharma] Add. Added customer-service-call-log-manager program's solution directory in scenario-based/customer-service-call-log-manager

---
+++ oops-csharp-program/scenario-based/customer-service-call-log-manager/ICallLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerServiceCallLogSystem
+{
+    internal interface ICallLog
+    {
+        void AddCallLog(List<CallLog> callLogs);
+        void SearchByKeyword(List<CallLog> callLogs);
+        void FilterByTime(List<CallLog> callLogs);
+    }
+}
---
+++ oops-csharp-program/scenario-based/customer-service-call-log-manager/Program.cs
@@ -0,0 +1,42 @@
+﻿using CustomerServiceCallLogSystem;
+using System;
+public class Program
+{
+    private List<CallLog> callLogs = new List<CallLog>();
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.StartApplication();
+    }
+    private void StartApplication()
+    {
+        ICallLogImpl iCallLogImpl = new ICallLogImpl();
+        // menu
+        Console.WriteLine("1.Add call log");
+        Console.WriteLine("2.Search by keyword");
+        Console.WriteLine("3.Filter By Time.");
+        Console.WriteLine("4.Exit");
+
+        while (true)
+        {
+            Console.WriteLine("Enter your choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if(choice == 4)
+            {
+                break;
+            }
+            switch (choice)
+            {
+                case 1:
+                    iCallLogImpl.AddCallLog(callLogs);
+                    break;
+                case 2:
+                    iCallLogImpl.SearchByKeyword(callLogs);
+                    break;
+                case 3:
+                    iCallLogImpl.FilterByTime(callLogs);
+                    break;
+            }
+        }
+    }
+}
