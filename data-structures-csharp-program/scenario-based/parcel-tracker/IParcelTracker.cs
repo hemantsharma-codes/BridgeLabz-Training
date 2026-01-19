ba37263fb6b78265223e13a3c43cdb48a975f1fa
hemantsharma-codes <golubhardwaj8126@gmail.com>
2026-01-19 15:17:44 +0530

    [Hemant Sharma] Add. Added the scenario-based program's of pracel tracker system in data-structures-csharp-program/scenario-based

---
+++ data-structures-csharp-program/scenario-based/parcel-tracker/IParcelTracker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzCopy.dsa_csharp_practice.scenario_based.ParcelTracker
+{
+    internal interface IParcelTracker
+    {
+        void AddParcel();
+        void AddStage();
+        void TrackStage();
+        void GetCurrentStatus();
+        void AddCustomIntermediateCheckPoint();
+        void DisplayAllParcel();
+    }
+}
---
+++ data-structures-csharp-program/scenario-based/parcel-tracker/StageNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzCopy.dsa_csharp_practice.scenario_based.ParcelTracker
+{
+    internal class StageNode
+    {
+        public string Status;
+        public StageNode Next;
+
+        public StageNode(string status)
+        {
+            this.Status = status;
+        }
+    }
+}
