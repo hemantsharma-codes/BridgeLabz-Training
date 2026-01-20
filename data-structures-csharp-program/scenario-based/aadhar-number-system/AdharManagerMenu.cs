97726acd900b615bd655a843e108e8e00b6d3d47
hemantsharma-codes <golubhardwaj8126@gmail.com>
2026-01-20 14:37:21 +0530

    [Hemant Sharma] Add. Added program's solution of aadhar number system

---
+++ data-structures-csharp-program/scenario-based/aadhar-number-system/AdharManagerMenu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzCopy.dsa_csharp_practice.scenario_based.SortAdhar
+{
+    internal class AdharManagerMenu
+    {
+        private IAadharManager _adharManager;
+        public AdharManagerMenu()
+        {
+            _adharManager = new AadharManagerUtitlity(10);
+        }
+        public void Run()
+        {
+            Console.WriteLine("=========Adhar Number System=============\n");
+
+            while (true)
+            {
+                Console.WriteLine("\n1. Add Aadhar");
+                Console.WriteLine("2. Display All Aadhar");
+                Console.WriteLine("3. Search Aadhar");
+                Console.WriteLine("0. Exit");
+
+                Console.WriteLine("Enter your choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        _adharManager.AddAadhar();
+                        break;
+                    case 2:
+                        _adharManager.DisplayAadhar();
+                        break;
+                    case 3:
+                        _adharManager.SearchAadhar();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting.....");
+                        return;
+                    default:
+                        Console.WriteLine("Please enter valid chioce");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ data-structures-csharp-program/scenario-based/aadhar-number-system/IAadharManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzCopy.dsa_csharp_practice.scenario_based.SortAdhar
+{
+    internal interface IAadharManager
+    {
+        void AddAadhar();
+
+        void DisplayAadhar();
+        void SearchAadhar();
+    }
+}
