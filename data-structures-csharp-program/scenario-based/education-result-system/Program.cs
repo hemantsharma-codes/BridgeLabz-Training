73791fc3c7e5d20efc1245b4fbcb87637f0bc8d8
hemantsharma-codes <golubhardwaj8126@gmail.com>
2026-01-21 15:47:29 +0530

    [Hemant Sharma] Add. Added program's solution of eduaction result system in scenario-based

---
+++ data-structures-csharp-program/scenario-based/education-result-system/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzCopy.dsa_csharp_practice.scenario_based.EduResults
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Run();
+        }
+    }
+}
---
+++ data-structures-csharp-program/scenario-based/education-result-system/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzCopy.dsa_csharp_practice.scenario_based.EduResults
+{
+    internal class Student
+    {
+        private int _studentsCount = 0;
+        private int _studentId;
+        private int _studentMarks;
+
+        // constructor
+        public Student(int studentMarks)
+        {
+
+            this._studentId = ++_studentsCount;
+            this._studentMarks = studentMarks;
+        }
+
+        // getter
+        public int GetStudentMarks()
+        {
+            return this._studentMarks;
+        }
+
+        // ToString() method overriding
+        public override string ToString()
+        {
+            return $"\nStudent Id : {_studentId}\nstudent Marks : {_studentMarks}";
+        }
+    }
+}
