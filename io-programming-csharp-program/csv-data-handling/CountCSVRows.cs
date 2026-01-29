944b8e95e5f9f7ded7e97fbe8712cfb96a5ea7f6
hemantsharma-codes <golubhardwaj8126@gmail.com>
2026-01-29 20:28:06 +0530

    [Hemant Sharma] Add : csv-data-hanling assignment's solution file

---
+++ io-programming-csharp-program/csv-data-handling/CountCSVRows.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzCopy.io_programming_csharp_program.csv_data_handling
+{
+    class CountCSVRows
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\golub\\Desktop\\BridgeLabzCopy\\BridgeLabzCopy\\BridgeLabzCopy\\io-programming-csharp-program\\csv-data-handling\\employees.csv";
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("CSV file not found.");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            int recordCount = lines.Length - 1;
+
+            Console.WriteLine("Total Employee Records: " + recordCount);
+        }
+    }
+
+}
---
+++ io-programming-csharp-program/csv-data-handling/WriteEmployeeCSV.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzCopy.io_programming_csharp_program.csv_data_handling
+{
+    class WriteEmployeeCSV
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\golub\\Desktop\\BridgeLabzCopy\\BridgeLabzCopy\\BridgeLabzCopy\\io-programming-csharp-program\\csv-data-handling\\employees.csv";
+
+            // Create employee data
+            string[] employeeData =
+            {
+            "ID,Name,Department,Salary",
+            "1,Rahul,IT,50000",
+            "2,Aditi,HR,45000",
+            "3,Suresh,Finance,55000",
+            "4,Neha,Marketing,48000",
+            "5,Aman,IT,60000"
+        };
+
+            // Write data to CSV file
+            File.WriteAllLines(filePath, employeeData);
+
+            Console.WriteLine("Employee data written successfully to CSV file.");
+        }
+    }
+
+}
